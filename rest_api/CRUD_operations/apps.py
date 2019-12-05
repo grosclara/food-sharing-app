f87ea94855b314f48fd9049103038f0fc7541aa3
Gros Clara <clara.gros@student-cs.fr>
2019-12-05 11:17:35 +0100

    Test creation of db django models

---
+++ rest_api/CRUD_operations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrudOperationsConfig(AppConfig):
+    name = 'CRUD_operations'
--- rest_api/api/settings.py
+++ rest_api/api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CRUD_operations.apps.CrudOperationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
