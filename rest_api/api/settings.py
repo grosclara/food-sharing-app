0beb2a3ead9220c01f4851c951df53ffcce50d6d
Babacar <babacar.toure@student-cs.fr>
2019-12-07 12:01:14 +0100

    sunday morning

--- rest_api/api/settings.py
+++ rest_api/api/settings.py
@@ -72,14 +72,26 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+} """
 
+# SQL Server Database Connection Properties
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'CShare',
+        'USER' : 'babacar',
+        'PASSWORD' : 'babacar',
+        'HOST' : 'localhost',
+        'PORT' : 3306,
+    }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
