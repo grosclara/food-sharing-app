7b424a7efa07f99abfc3cdef3a1ebdd21c27a4da
Gros Clara <clara.gros@student-cs.fr>
2019-12-07 11:36:58 +0100

    GetAllProducts Activity

--- CShare/app/src/main/java/com/example/cshare/MainActivity.java
+++ CShare/app/src/main/java/com/example/cshare/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toCollectActivity(View view) {
+        Intent toCollectActivityIntent = new Intent();
+        toCollectActivityIntent.setClass(getApplicationContext(), CollectActivity.class);
+        startActivity(toCollectActivityIntent);
+        finish();
     }
 }
