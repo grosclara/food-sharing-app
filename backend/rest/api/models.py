771bcfde126b20507da15a06d4b97af57cbd3500
Gros Clara <clara.gros@student-cs.fr>
2019-12-19 11:10:25 +0100

    django migrations

--- backend/rest/api/models.py
+++ backend/rest/api/models.py
@@ -7,6 +7,8 @@ class User(models.Model): # User table
     first_name = models.CharField(max_length=200) 
     objects = models.Manager() # A Manager is the interface through which database query operations are provided to Django models.
     profile_picture = models.ImageField(upload_to="media/user/", default='media/user/android.png')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Product(models.Model): # Product table
     name = models.CharField(max_length=200)
@@ -21,4 +23,6 @@ class Product(models.Model): # Product table
 class Order(models.Model): # Order table
     client = models.ForeignKey('User',on_delete=models.CASCADE)
     product = models.ForeignKey('Product',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
