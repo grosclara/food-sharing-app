99d2589f6b5ec2f830864930333f5c1fd389cce0
Babacar <babacar.toure@student-cs.fr>
2020-02-13 09:38:17 +0100

    admin

--- backend/rest/api/admin.py
+++ backend/rest/api/admin.py
@@ -11,13 +11,11 @@ from django.contrib.auth.admin import UserAdmin
 #this will be the custom admin
 class UserAdmin(UserAdmin):
     model = User
-    list_display = ()  # Contain only fields in your `custom-user-model`
-    list_filter = ()  # Contain only fields in your `custom-user-model` intended for filtering. Do not include `groups`since you do not have it
-    search_fields = ()  # Contain only fields in your `custom-user-model` intended for searching
-    ordering = ()  # Contain only fields in your `custom-user-model` intended to ordering
+    ordering = ()
     filter_horizontal = () # Leave it empty. You have neither `groups` or `user_permissions`
-    add_fieldsets = ((None, {'fields': ('email', 'password')}),('Personal info', {'fields': ('last_name','first_name','room_number','campus','profile_picture')}),('Permissions', {'fields': ('is_active','is_superuser','is_staff')}),)  
-  
+    add_fieldsets = ((None, {'fields': ('email','password1','password2')}),('Personal info', {'fields': ('last_name','first_name','date_joined','room_number','campus','profile_picture')}),('Permissions', {'fields': ('is_active','is_superuser','is_staff')}),)  
+    fieldsets = (('Personal info', {'fields': ('room_number','campus','profile_picture')}),('Permissions', {'fields': ('is_active','is_superuser','is_staff')}),)  
+
 
 
 # Indicates the tables on which the administrator can operate
