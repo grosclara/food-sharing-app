4c573c85845e841302a775bd69c7aaf8b1ce082a
Gros Clara <clara.gros@student-cs.fr>
2020-02-27 22:14:02 +0100

    gitignore add files

--- server/virtualenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\grosc\onedri~1\projet~2\projet~1\backend\virtua~1\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
