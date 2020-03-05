98116b615f9dd2ff1357876ff3a8af154c1a999a
Gros Clara <clara.gros@student-cs.fr>
2020-03-05 14:08:03 +0100

    recycle view completed

---
+++ cshare/app/src/main/java/com/example/cshare/WebServices/NetworkClient.java
@@ -0,0 +1,39 @@
+package com.example.cshare.WebServices;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class NetworkClient {
+
+    private static Retrofit retrofit;
+    public static final String BASE_URL = "http://10.0.2.2:8000/api/v1/";
+    //public static final String BASE_URL = "http://138.195.50.22:5040/api/v1/";
+
+    public static Retrofit getRetrofitClient(){
+        if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10,TimeUnit.SECONDS)
+                    .addInterceptor(logging)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    // Adapt to the Retrofit instance that will allow it to automatically convert its data into Observables
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
