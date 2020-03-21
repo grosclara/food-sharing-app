433a14b51808f51d1601dc9f4422ca94742b132a
Clara <clara.gros@student-cs.fr>
2020-03-21 14:43:28 +0100

    Add temporary variables (token and userId) in Constants

--- cshare/app/src/main/java/com/example/cshare/Utils/Constants.java
+++ cshare/app/src/main/java/com/example/cshare/Utils/Constants.java
@@ -4,4 +4,9 @@ public class Constants {
 
     public static final String TAG = "tag";
 
+    // Temp variables
+    public static final String TOKEN = "token 11d882f91e4bf9b410287932404186a7919c4ec1";
+    public static final int USERID = 5;
+
+
 }
