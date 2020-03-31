38d299bfbb82a3c486889314e623fda60b6413ae
Babacar <babacar.toure@student-cs.fr>
2020-03-31 17:13:03 +0200

    profile implemented

--- cshare/app/src/main/java/com/example/cshare/Controllers/Fragments/ProfileFragment.java
+++ cshare/app/src/main/java/com/example/cshare/Controllers/Fragments/ProfileFragment.java
@@ -4,7 +4,6 @@ package com.example.cshare.Controllers.Fragments;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,25 +17,39 @@ import android.widget.TextView;
 import com.example.cshare.Models.User;
 import com.example.cshare.R;
 import com.example.cshare.viewModels.ProfileViewModel;
-import com.example.cshare.databinding.MainBinder;
 import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
-
-    MainBinder binding;
 
     //ViewModel
     ProfileViewModel profileViewModel;
 
     View view;
 
+    //Views
+    private TextView textViewFirstName;
+    private TextView textViewLastName;
+    private TextView textViewEmail;
+    private TextView textViewCampus;
+    private TextView textViewRoomNumber;
+    private ImageView imageViewProfilePicture;
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // instantiate binder
-        binding = DataBindingUtil.setContentView(getActivity(),R.layout.fragment_profile);
-
+        //View
         view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        textViewFirstName = view.findViewById(R.id.textViewFirstName);
+        textViewLastName = view.findViewById(R.id.textViewLastName);
+        textViewEmail = view.findViewById(R.id.textViewEmail);
+        textViewCampus = view.findViewById(R.id.textViewCampus);
+        textViewRoomNumber = view.findViewById(R.id.textViewRoomNumber);
+        imageViewProfilePicture = view.findViewById(R.id.imageViewProfilePicture);
+
+
         configureViewModel();
         return view;
     }
@@ -45,16 +58,16 @@ public class ProfileFragment extends Fragment {
     protected void configureViewModel() {
         // Retrieve data from view model
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
-        binding.setProfileViewModel(profileViewModel);
         profileViewModel.getUserMutableLiveData().observe(getViewLifecycleOwner(), new Observer<User>() {
             @Override
-            public void onChanged(User user) {
-                binding.textViewFirstName.setText(user.getFirst_name());
-                binding.textViewLastName.setText(user.getLast_name());
-                binding.textViewEmail.setText(user.getEmail());
-                binding.textViewCampus.setText(user.getCampus());
-                binding.textViewRoomNumber.setText(user.getRoom_number());
-                Picasso.get().load(user.getProfile_picture()).into(binding.imageViewProfilePicture);
+            public void onChanged(User profile) {
+                textViewFirstName.setText(profile.getFirst_name());
+                textViewLastName.setText(profile.getLast_name());
+                textViewCampus.setText(profile.getCampus());
+                textViewEmail.setText(profile.getEmail());
+                textViewRoomNumber.setText(profile.getRoom_number());
+                Picasso.get().load(profile.getProfile_picture()).into(imageViewProfilePicture);
+
             }
         });
     }
