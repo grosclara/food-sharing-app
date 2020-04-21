bb4c386fc14c57d0bc35733d2aa3e01d23be7a98
Clara <clara.gros@student-cs.fr>
2020-04-21 16:10:05 +0200

    Change type NetworkError of error attribute in Throwable

--- cshare/app/src/main/java/com/example/cshare/Models/Auth/ResponseProductList.java
+++ cshare/app/src/main/java/com/example/cshare/Models/Auth/ResponseProductList.java
@@ -9,6 +9,7 @@ import com.example.cshare.WebServices.NetworkError;
 
 import java.util.List;
 
+import static com.example.cshare.RequestManager.Status.COMPLETE;
 import static com.example.cshare.RequestManager.Status.ERROR;
 import static com.example.cshare.RequestManager.Status.LOADING;
 import static com.example.cshare.RequestManager.Status.SUCCESS;
@@ -18,14 +19,14 @@ public class ResponseProductList {
     public final Status status;
 
     @Nullable
-    public final List<Product> data;
+    public final List<Product> productList;
 
     @Nullable
-    public final NetworkError error;
+    public final Throwable error;
 
-    private ResponseProductList(Status status, @Nullable List<Product> data, @Nullable NetworkError error) {
+    private ResponseProductList(Status status, @Nullable List<Product> productList, @Nullable Throwable error) {
         this.status = status;
-        this.data = data;
+        this.productList = productList;
         this.error = error;
     }
 
@@ -33,11 +34,15 @@ public class ResponseProductList {
         return new ResponseProductList(LOADING, null, null);
     }
 
-    public static ResponseProductList success(@NonNull List<Product> data) {
-        return new ResponseProductList(SUCCESS, data, null);
+    public static ResponseProductList success(@NonNull List<Product> productList) {
+        return new ResponseProductList(SUCCESS, productList, null);
     }
 
-    public static ResponseProductList error(@NonNull NetworkError error) {
+    public static ResponseProductList error(@NonNull Throwable error) {
         return new ResponseProductList(ERROR, null, error);
+    }
+
+    public static ResponseProductList complete(){
+        return new ResponseProductList(COMPLETE, null, null);
     }
 }
