6a9726e571607ace9cd80a0ddcf6674d00a16cbc
Clara <clara.gros@student-cs.fr>
2020-04-29 14:58:20 +0200

    Increase page size

---
+++ cshare/app/src/main/java/com/example/cshare/ViewModels/HomeViewModel.java
@@ -0,0 +1,36 @@
+package com.example.cshare.ViewModels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PageKeyedDataSource;
+import androidx.paging.PagedList;
+
+import com.example.cshare.Models.Product;
+import com.example.cshare.RequestManager.DataSourceFactories.HomeProductsDataSourceFactory;
+
+public class HomeProductViewModel extends ViewModel {
+    private LiveData<PagedList<Product>> productPagedList;
+    private LiveData<PageKeyedDataSource<Integer, Product>> liveDataSource;
+
+    public LiveData<PagedList<Product>> getProductPagedList() {
+        return productPagedList;
+    }
+
+    public LiveData<PageKeyedDataSource<Integer, Product>> getLiveDataSource() {
+        return liveDataSource;
+    }
+
+    public HomeProductViewModel() {
+        HomeProductsDataSourceFactory homeProductsDataSourceFactory = new HomeProductsDataSourceFactory();
+        liveDataSource = homeProductsDataSourceFactory.getHomeProductsLiveDataSource();
+
+        PagedList.Config config =
+                (new PagedList.Config.Builder())
+                        .setPageSize(15)
+                        .setEnablePlaceholders(false)
+                        .build();
+
+        productPagedList = (new LivePagedListBuilder<Integer, Product>(homeProductsDataSourceFactory, config)).build();
+    }
+}
