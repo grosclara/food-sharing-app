f660cbdec1f82ed3b0306c8d52988c6ff43cd932
Clara <clara.gros@student-cs.fr>
2020-05-14 15:00:33 +0200

    Add utils package-info.java file

--- cshare/app/src/main/java/com/example/cshare/utils/package-info.java
+++ cshare/app/src/main/java/com/example/cshare/utils/package-info.java
@@ -1,10 +1,5 @@
 /**
- * Define the REST API for Retrofit via the following interface and
- * create a Retrofit client.
- * <p>
- * Provides the various interfaces containing all the necessary CShare API endpoints. The instantiation of these interfaces is done via a Retrofit object (built in
- * {@link com.example.cshare.webservices.NetworkClient} class) which allows to build a
- * Retrofit service, used to make the calls.
+ * Provides common utility methods such as media files handling.
  *
  * @since 1.0
  * @author Clara Gros
